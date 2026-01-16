095981abb133c42479ed29ff827b14fec6b8f4c0
Omar Mahmoud <omar@Omars-MacBook-Pro-2.local>
2026-01-16 10:59:30 +0200

    fix base path for /xehpa

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
+  base: '/xehpa/',
   plugins: [
     react(),
     // Plugin to handle jszip as external
