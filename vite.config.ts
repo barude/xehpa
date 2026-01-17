b2f4e7a9b354a1ef238e49fa84d0da65a167f547
Omar Mahmoud <omar@Omars-MacBook-Pro-2.local>
2026-01-17 20:21:20 +0200

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/xehpa/' : '/',
+  base: '/',
   plugins: [
     react(),
     // Plugin to handle jszip as external
